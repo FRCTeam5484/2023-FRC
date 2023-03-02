53cc126793d226e7704da0e8e39da0b5d74c7765
RogerMCT <RogerMCT@Hotmail.com>
2023-03-02 07:09:23 -0500

    Updated Auto

--- src/main/java/frc/robot/commands/cmdAutonomous_DeadCode_PlaceConeDocked.java
+++ src/main/java/frc/robot/commands/cmdAutonomous_DeadCode_PlaceConeDocked.java
@@ -1,24 +1,21 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
-import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants.ArmAngleConstants;
 import frc.robot.Constants.ArmExtensionConstants;
 import frc.robot.subsystems.subArmAngle;
 import frc.robot.subsystems.subArmExtension;
-import frc.robot.subsystems.subClaw;
+import frc.robot.subsystems.subPneumatic;
 import frc.robot.subsystems.subSwerve;
 
 public class cmdAutonomous_DeadCode_PlaceConeDocked extends SequentialCommandGroup {
-  public cmdAutonomous_DeadCode_PlaceConeDocked(subSwerve swerve, subArmAngle angle, subArmExtension extension, subClaw claw) {
+  public cmdAutonomous_DeadCode_PlaceConeDocked(subSwerve swerve, subArmAngle angle, subArmExtension extension, subPneumatic air) {
     addCommands(
-      new ParallelRaceGroup(
-        new RunCommand(() -> claw.closeClaw(true), claw),
-        new cmdAuto_SetGoal(angle, extension, ArmAngleConstants.MidPosition, ArmExtensionConstants.MidPosition)
-      ).withTimeout(3),      
-      new RunCommand(() -> claw.openClaw(false), claw).withTimeout(1),
+      new cmdAuto_SetGoal(angle, extension, ArmAngleConstants.MidPosition, ArmExtensionConstants.MidPosition).withTimeout(3),      
+      new InstantCommand(() -> air.toggle(), air),
       new cmdAuto_SetDefault(angle, extension),
+      new InstantCommand(() -> air.toggle(), air),
       new cmdAutonomous_DeadCode_CrossLineDocked(swerve, 0.3)
     );
   }
